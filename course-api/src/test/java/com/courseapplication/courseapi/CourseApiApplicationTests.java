aa893a44a96f2a8c94b44cae40eedd682b940d49
himansi1994 <himansipatel1994@gmail.com>
2020-07-18 19:28:52 -0400

    **  Spring Application "course-api" created using Spring Boot  **      > It's for displaying courses related information

---
+++ course-api/src/test/java/com/courseapplication/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseapplication.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
