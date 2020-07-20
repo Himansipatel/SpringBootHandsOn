8a68489e40f47442b4495ff282e929adc79ca559
himansi1994 <himansipatel1994@gmail.com>
2020-07-20 15:20:23 -0400

    ** Spring Data JPA  project set up done **      > Connect spring application to relational database

---
+++ course-api-data/src/test/java/com/courseapplication/courseapidata/CourseApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseapplication.courseapidata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
