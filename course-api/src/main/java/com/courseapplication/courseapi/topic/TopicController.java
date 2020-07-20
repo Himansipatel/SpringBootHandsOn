5993302c962a64ca7436dfd95b400bd49698217e
himansi1994 <himansipatel1994@gmail.com>
2020-07-20 12:20:13 -0400

    ## service and controller added for updating topic information

--- course-api/src/main/java/com/courseapplication/courseapi/topic/TopicController.java
+++ course-api/src/main/java/com/courseapplication/courseapi/topic/TopicController.java
@@ -32,4 +32,9 @@ public class TopicController {
     public void addTopic(@RequestBody Topic topic){
         topicService.addTopic(topic);
     }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic,@PathVariable String id){
+        topicService.updateTopic(id,topic);
+    }
 }
--- course-api/src/main/java/com/courseapplication/courseapi/topic/TopicService.java
+++ course-api/src/main/java/com/courseapplication/courseapi/topic/TopicService.java
@@ -24,4 +24,14 @@ public class TopicService {
     public void addTopic(Topic topic){
         topics.add(topic);
     }
+
+    public void updateTopic(String id,Topic topic){
+        for (int i=0;i<topics.size();i++){
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)){
+                topics.set(i,topic);
+                return;
+            }
+        }
+    }
 }
