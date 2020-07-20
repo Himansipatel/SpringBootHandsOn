379180642f13374d134ee08ba13ffadd35e6d8c7
himansi1994 <himansipatel1994@gmail.com>
2020-07-20 11:40:25 -0400

    ## implemented GET and POST request      > Added request mapping to get all topic information      > Added method to add topic information\

--- course-api/src/main/java/com/courseapplication/courseapi/topic/TopicController.java
+++ course-api/src/main/java/com/courseapplication/courseapi/topic/TopicController.java
@@ -1,8 +1,7 @@
 package com.courseapplication.courseapi.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +13,23 @@ public class TopicController {
     private TopicService topicService;
 
     @RequestMapping("/topics")
-    public List<Topic> allTopics(){
+    public List<Topic> getAllTopics(){
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+//    //here foo is used rather than id so explicitely mention name of varable inside @PathVariable.but this is not recommended
+//    @RequestMapping("/topics/{foo}")
+//    public Topic getTopicDup(@PathVariable("foo") String id){
+//        return topicService.getTopic(id);
+//    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
 }
--- course-api/src/main/java/com/courseapplication/courseapi/topic/TopicService.java
+++ course-api/src/main/java/com/courseapplication/courseapi/topic/TopicService.java
@@ -3,18 +3,25 @@ package com.courseapplication.courseapi.topic;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("Spring","Spring Framework","spring framework description"),
             new Topic("Java","Java Framework","Java framework description"),
             new Topic("Python","Python Framework","Python framework description")
-    );
-   
+    ));
+
     public List<Topic> getAllTopics(){
         return topics;
+    }
+    public Topic getTopic(String id){
+        return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+    }
+    public void addTopic(Topic topic){
+        topics.add(topic);
     }
 }
